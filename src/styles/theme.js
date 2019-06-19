4afdb93d8842bb5472e60abd78cc22a936ee1f68
ebentz73 <evanbentz.dev@gmail.com>
2019-06-19 06:22:20 -0500

    chore(style): override input styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,16 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   shape: {
     borderRadius: 0
+  },
+  props: {
+    MuiTextField: {
+      fullWidth: true,
+      variant: 'outlined',
+      margin: 'dense'
+    },
+    MuiInputLabel: {
+      shrink: true
+    }
   }
 });
 

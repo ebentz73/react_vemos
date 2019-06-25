9e623113f7adc35de0a75d6b2f8c8bd2d41b66e3
ebentz73 <evanbentz.dev@gmail.com>
2019-06-25 10:45:18 -0500

    chore(style): use vemos colors in theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,6 +4,35 @@ const theme = createMuiTheme({
   shape: {
     borderRadius: 0
   },
+  palette: {
+    primary: {
+      main: '#333'
+    },
+    secondary: {
+      main: '#81b71a'
+    },
+    success: {
+      main: '#2d72ac'
+    },
+    info: {
+      main: '#9933CC'
+    },
+    warning: {
+      main: '#e37e1b'
+    },
+    danger: {
+      main: '#da4738'
+    },
+    text: {
+      primary: '#333'
+    },
+    background: {
+      default: '#EAEAEA',
+      dark: '#172B4D'
+    },
+    border: '#cccccc',
+    divider: '#cccccc'
+  },
   props: {
     MuiTextField: {
       fullWidth: true,

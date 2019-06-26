21fae2ddc64ea875cc3492f76b65ea7595bbac37
ebentz73 <evanbentz.dev@gmail.com>
2019-06-26 11:25:29 -0500

    chore(app): main app with default routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,119 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import { getAuth } from '@utils/firebase'; // eslint-disable-line
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Header from '@containers/layout/Header';
+import Notifier from '@containers/layout/Notifier';
+import Login from '@pages/Login';
+import Home from '@pages/Home';
+import AppActions, { AppSelectors } from '@redux/AppRedux';
+import { AuthSelectors } from '@redux/AuthRedux';
+
+const MainContainer = styled(Box)`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 class App extends Component {
-  render() {
+  static propTypes = {
+    loading: PropTypes.bool,
+    loaded: PropTypes.bool,
+    startup: PropTypes.func,
+    isLoggedIn: PropTypes.bool
+  };
+
+  componentDidMount() {
+    const { startup } = this.props;
+
+    startup();
+  }
+
+  renderAnonRoutes() {
     return (
-      <h1>
-        Hello world <Button variant="contained">Hello</Button>
-      </h1>
+      <Switch>
+        <Route exact path="/auth/login" component={Login} />
+        <Route render={() => <Redirect to="/auth/login" />} />
+      </Switch>
+    );
+  }
+
+  renderUserRoutes() {
+    return (
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route render={() => <Redirect to="/home" />} />
+      </Switch>
+    );
+  }
+
+  renderContent() {
+    const { isLoggedIn } = this.props;
+
+    if (!isLoggedIn) {
+      return this.renderAnonRoutes();
+    }
+
+    return this.renderUserRoutes();
+  }
+
+  renderLoading() {
+    return (
+      <Box
+        width={1}
+        p={4}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <CircularProgress color="secondary" />
+      </Box>
+    );
+  }
+
+  renderLoadingBar() {
+    const { loading } = this.props;
+
+    if (!loading) {
+      return null;
+    }
+
+    return (
+      <Box width={1}>
+        <LinearProgress color="secondary" />
+      </Box>
+    );
+  }
+
+  render() {
+    const { loaded } = this.props;
+
+    return (
+      <MainContainer>
+        <Header />
+        {this.renderLoadingBar()}
+        {loaded ? this.renderContent() : this.renderLoading()}
+        <Notifier />
+      </MainContainer>
     );
   }
 }
 
-export default App;
+const mapStatesToProps = state => ({
+  loading: AppSelectors.selectLoading(state),
+  loaded: AppSelectors.selectLoaded(state),
+  isLoggedIn: AuthSelectors.selectLoggedIn(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  startup: () => dispatch(AppActions.startup())
+});
+
+export default connect(
+  mapStatesToProps,
+  mapDispatchToProps
+)(App);

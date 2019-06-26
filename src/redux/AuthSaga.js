11edd945ec3716020eaa09857e95a16975b7c432
ebentz73 <evanbentz.dev@gmail.com>
2019-06-26 11:26:30 -0500

    feat(auth): custom token signin and refresh token

---
+++ src/redux/AuthSaga.js
@@ -0,0 +1,49 @@
+import { call, put } from 'redux-saga/effects';
+import jwtDecode from 'jwt-decode';
+import { getAuth } from '@utils/firebase';
+import logger from '@utils/logger';
+import { request } from './ApiSaga';
+import AuthActions from './AuthRedux';
+
+function signInCustomToken(token) {
+  const auth = getAuth();
+
+  return auth
+    .signInWithCustomToken(token)
+    .then(resp => resp)
+    .catch(e => {
+      throw new Error(e);
+    });
+}
+
+export function* refreshToken(token, venueId) {
+  const decoded = jwtDecode(token);
+  const result = yield call(
+    request,
+    'user/refresh-token',
+    {
+      token
+    },
+    {
+      user: decoded.uid,
+      venue: venueId
+    }
+  );
+  const currentVenue = result[venueId];
+
+  try {
+    yield call(signInCustomToken, currentVenue.token);
+
+    yield put(AuthActions.setToken(currentVenue.token));
+    yield put(
+      AuthActions.setCurrentVenue({
+        ...currentVenue,
+        id: venueId
+      })
+    );
+    yield put(AuthActions.setUser(decoded));
+  } catch (e) {
+    logger.error(e);
+    throw e;
+  }
+}

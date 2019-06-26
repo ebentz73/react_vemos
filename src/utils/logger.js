96c83e0487843ed62a5facfa4d7210a5c48c354b
ebentz73 <evanbentz.dev@gmail.com>
2019-06-26 13:59:48 -0500

    chore(app): added more logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,2 +1,10 @@
 // @TODO replace console with other
-export default console;
+
+const logger = {
+  debug: console.debug,
+  error: console.error,
+  log: console.log,
+  warn: console.warn
+};
+
+export default logger;

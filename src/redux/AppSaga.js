3feece0bf4a494ecbb561c34914052c3ca67b28f
ebentz73 <evanbentz.dev@gmail.com>
2019-06-26 14:34:42 -0500

    fix(app): startup loading from cookie

--- src/redux/AppSaga.js
+++ src/redux/AppSaga.js
@@ -4,22 +4,41 @@ import history from '@utils/history';
 import { refreshToken } from './AuthSaga';
 import AppActions from './AppRedux';
 import logger from '@utils/logger';
+import notifier from '@utils/notifier';
+import { getDomain } from '@utils/history';
 
 export function* startup() {
   yield put(AppActions.setLoading(true));
 
+  const domain = getDomain();
+  logger.debug('Current domain', domain);
+
   // loading auth data from cookie
-  const selectedVenue = Cookies.getJSON(process.env.WEB_APP_URL + '_selected');
-  const allVenues = Cookies.getJSON(process.env.WEB_APP_URL + '_venues');
+  try {
+    const selectedVenue = JSON.parse(
+      Cookies.get(process.env.WEB_APP_URL + '_selected', {
+        domain
+      })
+    );
+    const allVenues = JSON.parse(
+      Cookies.get(process.env.WEB_APP_URL + '_venues', {
+        domain
+      })
+    );
 
-  logger.debug('Selected Venue', selectedVenue);
-  logger.debug('All Venues', selectedVenue);
+    logger.debug('Selected Venue', selectedVenue);
+    logger.debug('All Venues', allVenues);
 
-  if (selectedVenue && allVenues) {
-    const { token, id } = selectedVenue;
+    if (selectedVenue && allVenues) {
+      const { token, id } = selectedVenue;
 
-    yield call(refreshToken, token, id);
-  } else {
+      yield call(refreshToken, token, id);
+    } else {
+      history.push('/auth/login');
+    }
+  } catch (e) {
+    logger.error(e);
+    notifier.error('There was problem logging in');
     history.push('/auth/login');
   }
 

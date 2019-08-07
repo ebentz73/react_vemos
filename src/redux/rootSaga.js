8a383da8ce95d2604e24e176a5a8b542044e8990
ebentz73 <evanbentz.dev@gmail.com>
2019-08-07 14:52:32 -0500

    feat(transaction): added transaction redux and saga

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,12 @@
 import { all, takeLatest } from 'redux-saga/effects';
 import { AppTypes } from './AppRedux';
+import { TransactionTypes } from './TransactionRedux';
 import { startup } from './AppSaga';
+import { loadTransactions } from './TransactionSaga';
 
 export default function* root() {
-  yield all([takeLatest(AppTypes.STARTUP, startup)]);
+  yield all([
+    takeLatest(AppTypes.STARTUP, startup),
+    takeLatest(TransactionTypes.LOAD_TRANSACTIONS, loadTransactions)
+  ]);
 }

5abe530a643a1d9e5e79c79a9dc4928b77009d7d
ebentz73 <evanbentz.dev@gmail.com>
2019-08-13 10:14:36 -0500

    feat(firebase): guest async select component

---
+++ src/components/firebase/GuestAsyncSelect.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { AuthSelectors } from '@redux/AuthRedux';
+import { AsyncSelect } from '@components/common';
+import { searchGuests } from '@api/FlashlightApi';
+
+GuestAsyncSelect.propTypes = {
+  label: PropTypes.string,
+  venueId: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+  TextFieldProps: PropTypes.object
+};
+
+function GuestAsyncSelect({
+  label,
+  onChange,
+  value,
+  TextFieldProps,
+  venueId,
+  ...rest
+}) {
+  const loadOptions = query => {
+    return searchGuests(venueId, query).then(result => {
+      console.log(result);
+      return result.hits;
+    });
+  };
+  return (
+    <AsyncSelect
+      loadOptions={loadOptions}
+      onChange={v => onChange(v)}
+      value={value}
+      getOptionLabel={guest =>
+        [`${guest.firstName} ${guest.lastName}`, guest.email, guest.phone]
+          .filter(t => t)
+          .join(' | ')
+      }
+      getOptionValue={guest => guest._id}
+      TextFieldProps={{
+        label,
+        variant: 'standard',
+        ...TextFieldProps
+      }}
+      {...rest}
+    />
+  );
+}
+
+const mapStatesToProps = state => ({
+  venueId: AuthSelectors.selectVenueId(state)
+});
+
+export default connect(mapStatesToProps)(GuestAsyncSelect);

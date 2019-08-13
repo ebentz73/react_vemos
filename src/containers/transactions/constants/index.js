284f355d4426b9ae97d38667170e4cc6ebc314d1
ebentz73 <evanbentz.dev@gmail.com>
2019-08-13 10:12:35 -0500

    chore(transaction): constants for txn

---
+++ src/containers/transactions/constants/index.js
@@ -0,0 +1,4 @@
+export { default as PAYMENT_METHODS } from './payment-methods';
+export { default as TRANSACTION_TYPES } from './transaction-types';
+export { default as HAS_RESERVATION } from './has-reservation';
+export { default as REFUNDED } from './refunded';
---
+++ src/containers/transactions/constants/refunded.js
@@ -0,0 +1,4 @@
+export default {
+  YES: 'Refunded',
+  NO: 'Not Refunded'
+};

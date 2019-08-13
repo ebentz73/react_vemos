b934de4f1dd8973fff31b1ddef219fed40991eea
ebentz73 <evanbentz.dev@gmail.com>
2019-08-13 10:12:13 -0500

    feat(common): async option select component

---
+++ src/components/common/AsyncOptionSelect.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { AuthSelectors } from '@redux/AuthRedux';
+import { Select } from '@components/common';
+
+AsyncOptionSelect.propTypes = {
+  label: PropTypes.string,
+  venueId: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+  loadOptions: PropTypes.func,
+  TextFieldProps: PropTypes.object
+};
+
+function AsyncOptionSelect({
+  label,
+  onChange,
+  value,
+  TextFieldProps,
+  loadOptions,
+  venueId,
+  ...rest
+}) {
+  const [options, setOptions] = useState([]);
+  const loadData = () => {
+    return loadOptions(venueId).then(items => setOptions(items));
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <Select
+      options={options}
+      onChange={onChange}
+      value={value}
+      getOptionValue={item => item._id}
+      TextFieldProps={{
+        label,
+        variant: 'standard',
+        ...TextFieldProps
+      }}
+      {...rest}
+    />
+  );
+}
+
+const mapStatesToProps = state => ({
+  venueId: AuthSelectors.selectVenueId(state)
+});
+
+export default connect(mapStatesToProps)(AsyncOptionSelect);
